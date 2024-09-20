4dc77ce582764c13978b15dacf241820f45568b9
MauriceDev1 <mrvolkwyn@gmail.com>
2024-09-20 14:08:30 +0200

    dashboard

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import Image from "next/image";
+import Link from "next/link";
+import Logo from "@/app/images/vecteezy_logo-element-mix_66617-removebg-preview (1).png"
+import Menu from "../components/Menu";
 
 export const metadata: Metadata = {
   title: "ELS | Company",
@@ -12,22 +15,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="w-full bg-gray-100 h-screen">
-      <div className="w-full bg-white h-16 border-b flex justify-end px-10">
-        <div className="w-96 bg-red-300 h-14 justify-end my-auto">
-          <div className="w-12 h-12 rounded-full bg-gray-200 my-1">
-            <Image
-              className="inline-block h-12 w-12 rounded-full ring-2 ring-white"
-              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-              alt="Profile Picture"
-              width={48} // Tailwind `h-12` (12 * 4px = 48px)
-              height={48} // Tailwind `w-12` (12 * 4px = 48px)
-              quality={80}
+    <div className="w-full flex h-screen">
+      {/* left */}
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] p-4 xl:w-[14%]">
+        <Link href="/" className="flex items-center justify-center gap-2">
+        <Image
+                alt="Product screenshot"
+                src={Logo}
+                width={2432}
+                height={1442}
+                className="w-12 h-12"
             />
-          </div>
-        </div>
+            <span className="hidden lg:block">Electronic Litigation System</span>
+        </Link>
+        <Menu />
       </div>
-        {children}
+      {/* right */}
+      <div className="w-[86%] md:w-[91%] lg:w-[84%] xl-[86%] bg-blue-100">
+          {children}
+      </div>
     </div>
   );
 }
